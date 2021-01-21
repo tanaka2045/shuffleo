06df2aa5731bc7de5f1c0b2d94bae2df04f9368c
Hideki Tanaka <tanaka2045@tbz.t-com.ne.jp>
2021-01-21 12:52:07 +0000

    match_offence_rev1

--- resources/views/shuffleout/match_offence.blade.php
+++ resources/views/shuffleout/match_offence.blade.php
@@ -4,28 +4,49 @@
 
   <div class="container-fluid">
     <div class="row">
-      <div class="col-lg-6 offset-lg-3 text-center">
+      <div class="col-lg-6 offset-lg-3 text-center px-0">
         <div class="font-o-lg txt-shadow my-2">対戦ルーム</div>
         <div class="row">
           {{-- 攻撃側のレイアウト開始 --}}
-          <div class="col-2 px-0">
-            
-          </div>
-          <div class="col-4 px-0">
-            <div class="text-center">
-            <a class="border-bottom font-o-esm txt-shadow" href="#!">あいう
-            </a>
-              <div class="mt-2"><img src="../images/back_red.png" style="max-width: 45%; height:auto;"></div>
-              <div><img src="../images/back_red.png" style="max-width: 45%; height:auto;"></div>
-              <div><img src="../images/back_red.png" style="max-width: 45%; height:auto;"></div>
-              <div><img src="../images/back_red.png" style="max-width: 45%; height:auto;"></div>
-              <div><img src="../images/back_red.png" style="max-width: 45%; height:auto;"></div>
+          <div class="col-6 px-0">
+            <div class="row">
+              <div class="col-8 offset-4 px-0 mb-2">
+                <a class="border-bottom font-o-esm txt-shadow" href="#!">あいう
+                </a>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-4 d-flex align-items-center justify-content-end pt-3 pt-3 px-0 mx-0">
+                <form action="">
+                <div class="form-group">
+                <select type="text" style="font-size: 11px; margin: 0px 0px 10px 10px;" name="diffencePoint" id="diffencePoint1">
+                  <option value="DNo1" selected>10</option>
+                  <option value="DNo2">20</option>
+                  <option value="DNo3">30</option>
+                  <option value="DNo4">40</option>
+                  <option value="DNo5">50</option>
+                </select>
+                </div>
+                </form>
+              </div>
+              <div class="col-8 px-0">
+                <img src="../images/back_red.png" style="max-width: 45%; height:auto;">
+              </div>
             </div>
           </div>
+          
+          {{--<div class="col-4 px-0">
+            <div class="text-center">
+            </a>
+              <div><img src="../images/back_red.png" style="max-width: 45%; height:auto;"></div>
+              <div><img src="../images/back_red.png" style="max-width: 45%; height:auto;"></div>
+              <div><img src="../images/back_red.png" style="max-width: 45%; height:auto;"></div>
+              <div><img src="../images/back_red.png" style="max-width: 45%; height:auto;"></div>
+            </div>--}}
         {{-- 攻撃側のレイアウト終了 --}}
         
         {{-- 守備側のレイアウト開始 --}}
-          <div class="col-4 px-0">
+          <div class="col-6 px-0">
             <div class="text-center">
             <a class="border-bottom font-o-esm txt-shadow mb-2" href="#!">ユーザ
             </a>
--- resources/views/shuffleout/user_home_test.blade.php
+++ resources/views/shuffleout/user_home_test.blade.php
@@ -4,224 +4,25 @@
     
   <div class="container-fluid">
     <div class="row">
-      <div class="col">
-        <div class="font-o-lg txt-shadow mt-3 mb-3">ユーザーホーム</div>
-      </div>
-    </div>
-    
-    <div class="row">
-      <div class="col  pl-0 mb-3">
-        <div class="font-o-md txt-shadow">カードステータス</div>
-        <div class="row">
-          {{-- 攻撃カードのレイアウト開始--}}
-          <div class="col-4 offset-2 px-0">
-            <div class="font-o-sm txt-shadow mt-2 mb-1" style="color: #FFCADC;">攻撃カード
-             </div>
-            <div class="row mx-0">
-              <div class="col-9 px-0">
-                <div class="font-o-sm">攻No1</div>
-              </div>
-            </div>
-            <div class="row mx-0">
-              <div class="col-9 offset-1 px-0">
-                <img src="../images/10_red.png" style="max-width: 80%; height:auto;">
-              </div>
-                <div class="col-2 px-0 pt-2">
-                  <div class="d-flex flex-column">
-                    <img src="../images/arrow_up.png" style="max-width: 50%; height:auto;">
-                    <br>
-                    <img src="../images/arrow_down.png" style="max-width: 50%; height:auto;">
-                  </div>
-                </div>
-            </div>
-            
-            <div class="row mx-0">
-              <div class="col-9 px-0">
-                <div class="font-o-sm">攻No2</div>
-              </div>
-            </div>
-            <div class="row mx-0">
-              <div class="col-9 offset-1 px-0">
-                <img src="../images/20_red.png" style="max-width: 80%; height:auto;">
-              </div>
-                <div class="col-2 px-0 pt-2">
-                  <div class="d-flex flex-column">
-                    <img src="../images/arrow_up.png" style="max-width: 50%; height:auto;">
-                    <br>
-                    <img src="../images/arrow_down.png" style="max-width: 50%; height:auto;">
-                  </div>
-                </div>
-            </div>            
- 
-             <div class="row mx-0">
-              <div class="col-9 px-0">
-                <div class="font-o-sm">攻No3</div>
-              </div>
-            </div>
-            <div class="row mx-0">
-              <div class="col-9 offset-1 px-0">
-                <img src="../images/30_red.png" style="max-width: 80%; height:auto;">
-              </div>
-                <div class="col-2 px-0 pt-2">
-                  <div class="d-flex flex-column">
-                    <img src="../images/arrow_up.png" style="max-width: 50%; height:auto;">
-                    <br>
-                    <img src="../images/arrow_down.png" style="max-width: 50%; height:auto;">
-                  </div>
-                </div>
-            </div>                   
- 
-             <div class="row mx-0">
-              <div class="col-9 px-0">
-                <div class="font-o-sm">攻No4</div>
-              </div>
-            </div>
-            <div class="row mx-0">
-              <div class="col-9 offset-1 px-0">
-                <img src="../images/40_red.png" style="max-width: 80%; height:auto;">
-              </div>
-                <div class="col-2 px-0 pt-2">
-                  <div class="d-flex flex-column">
-                    <img src="../images/arrow_up.png" style="max-width: 50%; height:auto;">
-                    <br>
-                    <img src="../images/arrow_down.png" style="max-width: 50%; height:auto;">
-                  </div>
-                </div>
-            </div>               
-                
-             <div class="row mx-0">
-              <div class="col-9 px-0">
-                <div class="font-o-sm">攻No5</div>
-              </div>
-            </div>
-            <div class="row mx-0">
-              <div class="col-9 offset-1 px-0">
-                <img src="../images/50_red.png" style="max-width: 80%; height:auto;">
-              </div>
-                <div class="col-2 px-0 pt-2">
-                  <div class="d-flex flex-column">
-                    <img src="../images/arrow_up.png" style="max-width: 50%; height:auto;">
-                    <br>
-                    <img src="../images/arrow_down.png" style="max-width: 50%; height:auto;">
-                  </div>
-                </div>
-            </div>
-            
-            <div class="row mx-0">
-              <div class="col-9 offset-1 px-0">
-                <div class="font-o-sm txt-shadow" style="color:#FFCADC;">計 150pt
-                </div>
-              </div>
-            </div>
-          </div>
-          {{-- 攻撃カードのレイアウト終了--}}
-          
-          {{-- 守備カードのレイアウト開始--}}
-          <div class="col-4 px-0">
-            <div  style="color: C9D3F6;" class="font-o-sm txt-shadow mt-2 mb-1">守備カード
-            </div>
-            <div class="row mx-0">
-              <div class="col-9 px-0">
-                <div class="font-o-sm">守No1</div>
-              </div>
-            </div>
-            <div class="row mx-0">
-              <div class="col-9 offset-1 px-0">
-                <img src="../images/10_blue.png" style="max-width: 80%; height:auto;">
-              </div>
-                <div class="col-2 px-0 pt-2">
-                  <div class="d-flex flex-column">
-                    <img src="../images/arrow_up.png" style="max-width: 50%; height:auto;">
-                    <br>
-                    <img src="../images/arrow_down.png" style="max-width: 50%; height:auto;">
-                  </div>
-                </div>
-            </div>
-            
-            <div class="row mx-0">
-              <div class="col-9 px-0">
-                <div class="font-o-sm">守No2</div>
-              </div>
-            </div>
-            <div class="row mx-0">
-              <div class="col-9 offset-1 px-0">
-                <img src="../images/20_blue.png" style="max-width: 80%; height:auto;">
-              </div>
-                <div class="col-2 px-0 pt-2">
-                  <div class="d-flex flex-column">
-                    <img src="../images/arrow_up.png" style="max-width: 50%; height:auto;">
-                    <br>
-                    <img src="../images/arrow_down.png" style="max-width: 50%; height:auto;">
-                  </div>
-                </div>
-            </div>            
- 
-             <div class="row mx-0">
-              <div class="col-9 px-0">
-                <div class="font-o-sm">守No3</div>
-              </div>
-            </div>
-            <div class="row mx-0">
-              <div class="col-9 offset-1 px-0">
-                <img src="../images/30_blue.png" style="max-width: 80%; height:auto;">
-              </div>
-                <div class="col-2 px-0 pt-2">
-                  <div class="d-flex flex-column">
-                    <img src="../images/arrow_up.png" style="max-width: 50%; height:auto;">
-                    <br>
-                    <img src="../images/arrow_down.png" style="max-width: 50%; height:auto;">
-                  </div>
-                </div>
-            </div>                   
- 
-             <div class="row mx-0">
-              <div class="col-9 px-0">
-                <div class="font-o-sm">守No4</div>
-              </div>
-            </div>
-            <div class="row mx-0">
-              <div class="col-9 offset-1 px-0">
-                <img src="../images/40_blue.png" style="max-width: 80%; height:auto;">
-              </div>
-                <div class="col-2 px-0 pt-2">
-                  <div class="d-flex flex-column">
-                    <img src="../images/arrow_up.png" style="max-width: 50%; height:auto;">
-                    <br>
-                    <img src="../images/arrow_down.png" style="max-width: 50%; height:auto;">
-                  </div>
-                </div>
-            </div>               
-                
-             <div class="row mx-0">
-              <div class="col-9 px-0">
-                <div class="font-o-sm">守No5</div>
-              </div>
-            </div>
-            <div class="row mx-0">
-              <div class="col-9 offset-1 px-0">
-                <img src="../images/60_blue.png" style="max-width: 80%; height:auto;">
-              </div>
-                <div class="col-2 px-0 pt-2">
-                  <div class="d-flex flex-column">
-                    <img src="../images/arrow_up.png" style="max-width: 50%; height:auto;">
-                    <br>
-                    <img src="../images/arrow_down.png" style="max-width: 50%; height:auto;">
-                  </div>
-                  
-                </div>
-            </div>
-            
-            <div class="row mx-0">
-              <div class="col-9 offset-1 px-0">
-                <div class="font-o-sm txt-shadow"  style="color: #C9D3F6;">計 160pt
-                </div>
-              </div>
-            </div>
-           {{-- 守備カードのレイアウト終了--}}           
+      <div class="col-4 offset-2 d-flex align-items-center justify-content-end pt-3">
+        <form action="">
+        <div class="form-group "> 
+        <div>
+          <select type="text" name="diffencePoint" id="diffencePoint1">
+            <option value="DNo1" selected>10</option>
+            <option value="DNo2">20</option>
+            <option value="DNo3">30</option>
+            <option value="DNo4">40</option>
+            <option value="DNo5">50</option>
+          </select>
           </div>
         </div>
+        </form>
       </div>
-  　</div>
+      <div class="col-6 text-left pt-3">
+        <img src="../images/back_red.png" style="max-width: 45%; height:auto;">
+      </div>
+    </div>
   </div>
 @endsection
  
