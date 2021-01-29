0fc68fbf59058401a27a92a6aeb4cf8c15fd4cc3
Hideki Tanaka <tanaka2045@tbz.t-com.ne.jp>
2021-01-29 01:06:11 +0000

    user login creating Rev0

--- resources/views/users/match_make.blade.php
+++ resources/views/users/match_make.blade.php
@@ -10,9 +10,9 @@
     </div>
     
     <div class="row">
-      <div class="col-lg-8 offset-lg-2 mt-2">
-        <div class="p-2 bg-nav-base btn-shadow" style="border:1px solid #FFFFFF; border-radius:0.2rem; ">
-          <div class="font-o-elg txt-shadow text-left pl-3" style="color: #C9D3F6;">守備側で対戦</div>
+      <div class="col-lg-8 offset-lg-2 mt-2 px-4">
+        <div class="p-2 bg-green btn-shadow" style="border:1px solid #FFFFFF; border-radius:0.2rem; ">
+          <div class="font-o-elg txt-shadow text-left pl-2" style="color: #C9D3F6;">守備で対戦</div>
           <div class="text-right">
             <a href="{{ action('Users\MatchController@matchDiffenceAccess') }}" role="button" tabindex="0" class="btn btn-diffence btn-shadow font-o-esm">新規対戦ルーム登録</a>
           </div>          
@@ -21,21 +21,21 @@
     </div>
     
     <div class="row">
-      <div class="col-lg-8 offset-lg-2 mt-5">
-         <div class="p-2 bg-nav-base btn-shadow" style="border:1px solid #FFFFFF; border-radius:0.2rem; ">
-          <div class="font-o-elg txt-shadow text-left pl-3" style="color:#FFCADC;">攻撃側で対戦</div>
+      <div class="col-lg-8 offset-lg-2 my-5 px-4">
+         <div class="p-2 bg-green btn-shadow" style="border:1px solid #FFFFFF; border-radius:0.2rem; ">
+          <div class="font-o-elg txt-shadow text-left pl-2" style="color:#FFCADC;">攻撃で対戦</div>
           
           <table class="table table-sm mt-3 mb-2">
             <thead>
               <tr>
-                <th class="align-middle font-o-md" style="border: none;">ユーザー名</th>
+                <th class="text-left align-middle font-o-md pl-3" style="border: none;">ユーザー名</th>
                 <th class="align-middle font-o-esm" style="border: none;">入出中<br>ユーザー</th>
                 <th style="border: none;"></th>
               </tr>
             </thead>             
             <tbody>
               <tr>
-                <th scope="row" class="font-o-md txt-shadow align-middle p-0">user0017</th>
+                <th scope="row" class="font-o-md text-left align-middle pl-3">user0017</th>
                 <td class="font-o-md align-middle">2</td>
                 <td>
                   <div class="btn-group-vertical">
@@ -44,17 +44,17 @@
                 </td>
               </tr>
               <tr>
-                <th scope="row" class="font-o-md txt-shadow align-middle p-0">あいうえおかきくけこ</th>
+                <th scope="row" class="font-o-md text-left align-middle pl-3">あいうえおかきくけこ</th>
                 <td class="font-o-md align-middle">2</td>
                 <td>
                   <div class="btn-group-vertical">
                     <a href="{{ action('Users\MatchController@matchOffenceAccess') }}" role="button" tabindex="0" class="btn btn-offence btn-shadow font-o-esm mx-0 mt-1 px-1">対戦ルームへ</a>
-                    <button type="button" class="btn btn-green btn-shadow font-o-esm mx-0 mb-1 px-1">キャンセル</button>
+                    <button type="button" class="btn btn-cancel-red btn-shadow font-o-esm mx-0 my-1 px-1" style="color:#A3002F;">キャンセル</button>
                   </div>
                 </td>
               </tr>
               <tr>
-                <th scope="row" class="font-o-md txt-shadow align-middle p-0">user0213</th>
+                <th scope="row" class="font-o-md text-left align-middle pl-3">user0213</th>
                 <td class="font-o-md align-middle">10</td>
                 <td>
                   <div class="btn-group-vertical">
