34b69b5c1f47ca2cbf0e4ddfb3250a64e3b3d296
Hideki Tanaka <tanaka2045@tbz.t-com.ne.jp>
2021-01-30 10:46:13 +0000

    original login creating Rev3

--- resources/views/users/user_register_2.blade.php
+++ resources/views/users/user_register_2.blade.php
@@ -13,10 +13,10 @@
   
     <div class="row">
       <div class="col-lg-6 offset-lg-3 font-weight-bold" style="text-align: left;">
-        
-        {{-- ニックネームの登録 --}}
         <form method="POST" action="{{ route('register') }}">
           @csrf
+        
+          {{-- ニックネームの登録 --}}
           <div class="form-group row mx-0">
             <label for="nickname">{{ __('【ニックネーム】' ) }}</label>
             <input id="nickname" type="text" class="form-control mx-2 @error('nickname') is-invalid @enderror" maxlength="10" name="nickname" value="{{ old('nickname') }}" required autocomplete="nickname" autofocus 
@@ -27,54 +27,46 @@
               </span>
             @enderror
           </div>
-        </form>
         
-        {{-- 性別の登録 --}}
-        <form method="POST" action="{{ route('register') }}">
-          @csrf
+          {{-- 性別の登録 --}}
           <div>
             <label for="gender">{{ __('【性別】' ) }}</label>
           </div>
           <span class="radio-inline ml-2 mr-4">
             <label>
-              <input type="radio" name="gender" value="male" {{--@if(Input::old('gender')=="male") checked @endif--}}> 男
+              <input type="radio" name="gender" value="male" {{ old('gender','male') == 'male' ? 'checked' : '' }}> 男
             </label>
           </span>
           <span class="radio-inline">
             <label>
-              <input id="gender" type="radio" name="gender" value="female" {{--@if(Input::old('gender')=="female") checked @endif--}}> 女
+              <input id="gender" type="radio" name="gender" value="female" {{ old('gender')? 'checked' : '' }}> 女
             </label>
           </span>
           <span class="form-group @if(!empty($errors->first('gender'))) has-error @endif">
             <span class="help-block">{{$errors->first('gender')}}</span>
           </span>
-        </form>
         
-        {{-- 年代の登録 --}}
-        <form method="POST" action="{{ route('register') }}">
-          @csrf
+          {{-- 年代の登録 --}}
           <div class="form-group @if(!empty($errors->first('age'))) has-error @endif" >
             <div>
               <label for="age">{{ __('【年代】' ) }}</label>
             </div>
             <select id="age" type="text" class="mx-2" name="age" id="age" style="background-color:#000000; color:#FFFFFF;">
               <option value=""></option>
-              <option value="20歳未満">20歳未満</option>
+              <option value="10代">10代</option>
               <option value="20代">20代</option>
               <option value="30代">30代</option>
               <option value="40代">40代</option>
               <option value="50代">50代</option>
               <option value="60代">60代</option>
               <option value="70代">70代</option>
-              <option value="80歳以上">80歳以上</option>
+              <option value="80代">80代</option>
+              <option value="90代">90代</option>
             </select>
             <span class="help-block">{{$errors->first('area')}}</span>
           </div>
-        </form>
         
-        {{-- ログインIDの登録 --}}
-        <form method="POST" action="{{ route('register') }}">
-          @csrf
+          {{-- ログインIDの登録 --}}
           <div class="form-group row mx-0">
             <label for="name">{{ __('【ログインID】' ) }}</label>
             <input id="name" type="text" class="form-control mx-2 @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" placeholder="6文字以上で入力" style="height: 1.5em; background-color:#000000; color:#FFFFFF;">
@@ -84,11 +76,8 @@
               </span>
             @enderror
           </div>
-        </form>
         
-        {{-- パスワードの登録 --}}
-        <form method="POST" action="{{ route('register') }}">
-          @csrf
+          {{-- パスワードの登録 --}}
           <div class="form-group row mx-0">
             <label for="password">{{ __('【パスワード】' ) }}</label>
             <input id="password" type="text" class="form-control mx-2 @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="6文字以上で入力" style="height: 1.5em; background-color:#000000; color:#FFFFFF;">
@@ -98,20 +87,14 @@
               </span>
             @enderror
           </div>
-        </form>
         
-        {{-- パスワード確認 --}}
-        <form method="POST" action="{{ route('register') }}">
-          @csrf
+          {{-- パスワード確認 --}}
           <div class="form-group row mx-0">
             <label for="password-confirm">{{ __('【パスワード（確認用）】' ) }}</label>
             <input id="password-confirm" type="text" class="form-control mx-2 @error('password') is-invalid @enderror" name="password_confirmation" required autocomplete="new-password" style="height: 1.5em; background-color:#000000; color:#FFFFFF;">
           </div>
-        </form>
 
-        {{-- メールアドレスの登録 --}}
-        <form method="POST" action="{{ route('register') }}">
-          @csrf
+          {{-- メールアドレスの登録 --}}
           <div class="form-group row mx-0">
             <label for="email">{{ __('【メールアドレス】' ) }}</label>
             <input id="email" type="email" class="form-control mx-2 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="メールアドレス" style="height: 1.5em; background-color:#000000; color:#FFFFFF;">
@@ -121,16 +104,17 @@
               </span>
             @enderror
           </div>
+          
+          <div class="row">
+            <div class="col text-center my-5">
+              <button type="submit" class="btn btn-blue text-center btn-shadow">{{ __('登録') }}</button>
+            </div>
+          </div>
+          
         </form>
-        
       </div>
     </div>
     
-    <div class="row">
-      <div class="col text-center my-3">
-        <button type="button" class="btn btn-blue text-center btn-shadow">登録</button>
-      </div>
-    </div>
     
   </div>
   
