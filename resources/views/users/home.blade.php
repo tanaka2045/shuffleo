94e69c5b5db2959169aadfa257d36e6ba493551e
Hideki Tanaka <tanaka2045@tbz.t-com.ne.jp>
2021-02-03 04:37:16 +0000

    admin announce creating Rev0

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -12,9 +12,9 @@
       </div>
     </div>
   
-    <div class="row">
+    <div class="row mx=0">
       <div class="col-lg-6 offset-lg-3 text-center mt-5">
-        <a href="{{ action('Users\HelpMessageController@announceAccess') }}" class="font-o-md txt-shadow border-bottom" style="margin-bottom: 20px;">インフォメーション</a>
+        <a href="{{ action('Users\HelpMessageController@announceAccess') }}" class="font-o-md border-bottom" style="margin-bottom: 20px;">インフォメーション</a>
         <div class="cardtext-black border-0 mt-3" style="background-color: transparent;">
           <h5 class="font-o-sm">21/1/13　　作成中　まだ遊べません</h5>
           <h5 class="font-o-sm">21/1/12　　コメントサンプルYYY</h5>
@@ -28,12 +28,28 @@
         </div>
       </div>
     </div>
-    <div class="row">
+    <div class="row mx-0">
       <div class="col text-center">
-        <div class="btn-group-vertical">
-          <a href="{{ action('Users\StatisticController@rankingTotalAccess') }}" role="button" class="btn btn-blue btn-shadow mt-4" style="text-align: center;">ランキング</a>
-          <a href="{{ action('Users\MatchController@matchHistoryAccess') }}" role="button"  class="btn btn-blue btn-shadow mb-2" style="text-align: center;">対戦履歴</a>
+        <div class="btn-group-vertical my-4">
+          <a href="{{ action('Users\StatisticController@rankingTotalAccess') }}" role="button" class="btn btn-blue btn-shadow mx-0" style="text-align: center;">{{ __('ランキング') }}</a>
+          <a href="{{ action('Users\MatchController@matchHistoryAccess') }}" role="button"  class="btn btn-blue btn-shadow mx-0" style="text-align: center;">{{ __('対戦履歴') }}</a>
         </div>
+      </div>
+    </div>
+        
+    <div class="row mx-0">
+      <div class="col font-o-sm mb-5">
+        <a href="{{ route('logout') }}"
+           onclick="event.preventDefault();
+                         document.getElementById('logout-form').submit();" class="border-bottom" style="color:#DEEBF7;">{{ __('ログアウト') }}
+        </a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+          @csrf
+        </form>
+        </div> 
+    </div>
+        
+        
       </div>
     </div>
   </div>
